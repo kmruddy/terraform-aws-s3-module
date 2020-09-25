110f78fc24d0a92d5ed85a1a7d9f1f5b4c0f5071
Kyle Ruddy <kmruddy@users.noreply.github.com>
2020-09-25 13:53:48 -0400

    Update main.tf
    roll back

--- main.tf
+++ main.tf
@@ -1,34 +1,9 @@
-data "aws_subnet" "default" {
-  id = var.subnet_id
-}
-
-data "aws_security_group" "elb" {
-  id = var.sg_id
-}
-
-resource "random_pet" "elb_name" {
+resource "random_pet" "bucket_name" {
   length    = 2
   separator = "-"
 }
 
-resource "aws_elb" "web" {
-  name = "kr-${var.name}-${random_pet.elb_name.id}"
-
-  subnets         = [data.aws_subnet.default.id]
-  security_groups = [data.aws_security_group.elb.id]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  idle_timeout                = var.idle_timeout
-  connection_draining         = var.connection_draining
-  connection_draining_timeout = var.connection_draining_timeout
-
-  tags = {
-    Name = var.tag_name
-  }
+resource "aws_s3_bucket" "random_bucket" {
+  bucket = "terraform-webinar-${random_pet.bucket_name.id}"
+  region = var.aws_region
 }
